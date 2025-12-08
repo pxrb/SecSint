c23d7ebcbd48cbb1366b3b9e1084c3c55929e880
Pxrb <jdjeifhdh@proton.me>
2025-12-08 12:48:16 -0800

    added modules

--- installer.bat
+++ installer.bat
@@ -1,9 +1,13 @@
 @echo off
 REM Install colorama
 pip install colorama
+pip install socket
+pip install requests
+pip install dnspython
 
 REM Run your Python script
 python Toolkit.py
 
 REM Pause so you can see any output
 pause
+
